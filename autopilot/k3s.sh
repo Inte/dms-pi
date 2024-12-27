dbd4318354d117f28250829a1bbbc7de315ff0cc
Tobias Kral <tobias@kral.ws>
2024-12-27 09:13:43 +0100

    K3s Installation & Token Ausgabe ergänzt

--- autopilot/k3s.sh
+++ autopilot/k3s.sh
@@ -23,3 +23,19 @@ else
     echo "Fehler beim Herunterladen oder Speichern der config.yaml."
     exit 1
 fi
+
+# Installation von K3s
+K3S_INSTALL_SCRIPT="https://get.k3s.io"
+echo "Starte Installation von K3s..."
+sudo curl -sfL "$K3S_INSTALL_SCRIPT" | sh - || { echo "Fehler: K3s-Installation fehlgeschlagen."; exit 1; }
+
+# Ausgabe des generierten Token
+TOKEN_PATH="/mnt/hat/k3s/server/token"
+echo "Automatisch generiertes Token in $TOKEN_PATH"
+
+if [ -f "$TOKEN_PATH" ]; then
+    sudo cat "$TOKEN_PATH" || { echo "Fehler: Token konnte nicht gelesen werden."; exit 1; }
+else
+    echo "Fehler: Token-Datei nicht gefunden unter $TOKEN_PATH."
+    exit 1
+fi
